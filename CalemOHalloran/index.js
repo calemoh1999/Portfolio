6d7866d950a54ad356029efde36d9e773afe9fa4
calemoh1999 <calemohalloran99@gmail.com>
2023-07-31 18:01:02 +0100

    Update index.js

--- CalemOHalloran/index.js
+++ CalemOHalloran/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
+const path = require('path');
 
 const app = express();
 dotenv.config();
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'CalemOHalloran')));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
