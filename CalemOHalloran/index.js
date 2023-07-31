1fa92547a8acd1a0535780e0ee900be9faa1c1ce
calemoh1999 <calemohalloran99@gmail.com>
2023-07-31 17:29:09 +0100

    a lot

---
+++ CalemOHalloran/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.post('/send-email', (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: 'calemohalloran99@gmail.com', // Replace with your recipient email address
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Error sending email' });
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json({ message: 'Email sent successfully' });
+    }
+  });
+});
