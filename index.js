174f231b7a172faed673e96c0e4e10a3f3f21b1d
calemoh1999 <calemohalloran99@gmail.com>
2023-07-31 20:01:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+// index.js
 const express = require('express');
 const nodemailer = require('nodemailer');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -9,12 +9,9 @@ const port = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve static files from the "CalemOHalloran" folder
-app.use(express.static(path.join(__dirname, 'CalemOHalloran')));
-
 // Serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'CalemOHalloran', 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 
 // Route to handle form submission
@@ -23,16 +20,16 @@ app.post('/submit', (req, res) => {
 
   // Use Nodemailer to send the email
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', // e.g., Gmail, Outlook, etc.
+    service: 'Gmail',
     auth: {
-      user: 'portfolioformscalem@gmail.com', // Your email address
-      pass: 'kpg hdnr triq rvxbt', // Your email password or an application-specific password
+      user: 'portfolioformscalem@gmail.com', // Replace this with your Gmail email address
+      pass: 'knatkfadqkkkwxes', // Replace this with the generated app password
     },
   });
 
   const mailOptions = {
     from: email,
-    to: 'portfolioformscalem@gmail.com', // Replace this with your email address where you want to receive the messages
+    to: 'calemohalloran99@gmail.com', // Replace this with your email address where you want to receive the messages
     subject: 'New Form Submission',
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
