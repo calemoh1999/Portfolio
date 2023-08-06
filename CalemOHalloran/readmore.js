a04137df6bee08c0bcaacc15de8798514cccc0ac
calemoh1999 <calemohalloran99@gmail.com>
2023-08-07 00:36:22 +0100

    m

--- CalemOHalloran/readmore.js
+++ CalemOHalloran/readmore.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     const readMoreLink = document.querySelector(".read-more-link");
     const hiddenContent = document.querySelector(".hidden-content");
-  
+    const initialHeight = 200; // Adjust the initial height as needed
+
     // Function to toggle the visibility of the hidden content
     function toggleHiddenContent() {
-      hiddenContent.classList.toggle("expanded");
-      if (hiddenContent.classList.contains("expanded")) {
-        readMoreLink.textContent = "Read Less";
-        hiddenContent.style.maxHeight = hiddenContent.scrollHeight + "px";
-      } else {
-        readMoreLink.textContent = "Read More";
-        hiddenContent.style.maxHeight = ""; // Remove the maxHeight property
-      }
+        hiddenContent.classList.toggle("expanded");
+        if (hiddenContent.classList.contains("expanded")) {
+            readMoreLink.textContent = "Read Less";
+            hiddenContent.style.maxHeight = hiddenContent.scrollHeight + "px";
+        } else {
+            readMoreLink.textContent = "Read More";
+            hiddenContent.style.maxHeight = initialHeight + "px";
+        }
     }
-  
+
+    // Show/hide content based on the initial height
+    hiddenContent.style.maxHeight = initialHeight + "px";
+
     // Show "Read More" link if content exceeds initial height
-    if (hiddenContent.scrollHeight > hiddenContent.clientHeight) {
-      readMoreLink.style.display = "inline";
+    if (hiddenContent.scrollHeight > initialHeight) {
+        readMoreLink.style.display = "inline";
     }
-  
+
     // Toggle content visibility when "Read More" link is clicked
     readMoreLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      toggleHiddenContent();
+        event.preventDefault();
+        toggleHiddenContent();
     });
-  });
-  
+});
