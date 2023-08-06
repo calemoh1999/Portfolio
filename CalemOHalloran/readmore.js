8fe8917a99d1193149a32b37c50db2d2811122aa
calemoh1999 <calemohalloran99@gmail.com>
2023-08-07 00:03:03 +0100

    n

--- CalemOHalloran/readmore.js
+++ CalemOHalloran/readmore.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     const readMoreLink = document.querySelector(".read-more-link");
-    const hiddenContent = document.querySelector(".hidden-content");
     const content = document.querySelector(".content");
   
     readMoreLink.addEventListener("click", function (event) {
       event.preventDefault();
-      hiddenContent.classList.toggle("expanded");
-      if (hiddenContent.classList.contains("expanded")) {
+      content.classList.toggle("expanded");
+      if (content.classList.contains("expanded")) {
         readMoreLink.textContent = "Read Less";
-        content.style.maxHeight = content.scrollHeight + "px";
       } else {
         readMoreLink.textContent = "Read More";
-        content.style.maxHeight = "200px"; // Adjust the height back to the original value
       }
     });
   });
