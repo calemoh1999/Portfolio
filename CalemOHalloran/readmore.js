db486390c050e05bc077f9d91d3310a7c4c7dee3
calemoh1999 <calemohalloran99@gmail.com>
2023-08-07 00:05:35 +0100

    Update readmore.js

--- CalemOHalloran/readmore.js
+++ CalemOHalloran/readmore.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const readMoreLink = document.querySelector(".read-more-link");
     const content = document.querySelector(".content");
+    const hiddenContent = document.querySelector(".hidden-content");
   
     readMoreLink.addEventListener("click", function (event) {
       event.preventDefault();
       content.classList.toggle("expanded");
+  
       if (content.classList.contains("expanded")) {
         readMoreLink.textContent = "Read Less";
+        content.style.maxHeight = hiddenContent.clientHeight + "px";
       } else {
         readMoreLink.textContent = "Read More";
+        content.style.maxHeight = "200px"; // Adjust the height back to the original value
       }
     });
   });
