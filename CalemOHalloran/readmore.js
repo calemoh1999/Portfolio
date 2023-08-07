0779c3f1b1a9729a50f4de37879e8484c15bc7be
calemoh1999 <calemohalloran99@gmail.com>
2023-08-07 20:12:37 +0100

    m

--- CalemOHalloran/readmore.js
+++ CalemOHalloran/readmore.js
@@ -1,25 +1,23 @@
 function myFunction(dotsId, moreTextId, btnId) {
-  var dots = document.getElementById(dotsId);
-  var moreText = document.getElementById(moreTextId);
-  var btnText = document.getElementById(btnId);
-
-  if (moreText.style.maxHeight === "" || moreText.style.maxHeight === "0px") {
-    // Expand the content
-    btnText.innerHTML = "Read less"; 
-    moreText.style.maxHeight = moreText.scrollHeight + "px";
-  } else {
-    // Collapse the content
-    btnText.innerHTML = "Read more"; 
-    moreText.style.maxHeight = "0";
+    var dots = document.getElementById(dotsId);
+    var moreText = document.getElementById(moreTextId);
+    var btnText = document.getElementById(btnId);
+  
+    if (dots.style.display === "none" || dots.style.display === "") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
   }
-}
-
-// Add an event listener to execute the function after the DOM has loaded
-document.addEventListener("DOMContentLoaded", function () {
-  // Call the function for each read more button
-  myFunction('dots1', 'more1', 'myBtn1');
-  myFunction('dots2', 'more2', 'myBtn2');
-  myFunction('dots3', 'more3', 'myBtn3');
-});
-
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    // Call the function for each read more button
+    myFunction('dots1', 'more1', 'myBtn1');
+    myFunction('dots2', 'more2', 'myBtn2');
+    myFunction('dots3', 'more3', 'myBtn3');
+  });
   
