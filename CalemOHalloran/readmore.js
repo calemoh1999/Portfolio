6e45adccb5b4f843015e46d4ff15a7f883388890
calemoh1999 <calemohalloran99@gmail.com>
2023-08-07 20:37:42 +0100

    Update readmore.js

--- CalemOHalloran/readmore.js
+++ CalemOHalloran/readmore.js
@@ -1,24 +1,22 @@
 function myFunction(dotsId, moreTextId, btnId) {
   var dots = document.getElementById(dotsId);
   var moreText = document.getElementById(moreTextId);
-  var btnText = document.querySelector("." + btnId); // Use querySelector to select by class name
-  
+  var btnText = document.getElementById(btnId);
+
   if (dots.style.display === "none" || dots.style.display === "") {
     dots.style.display = "inline";
-    btnText.textContent = "Read more"; // Use textContent instead of innerHTML for simple text changes
+    btnText.innerHTML = "Read more"; 
     moreText.style.display = "none";
   } else {
     dots.style.display = "none";
-    btnText.textContent = "Read less"; // Use textContent instead of innerHTML for simple text changes
+    btnText.innerHTML = "Read less"; 
     moreText.style.display = "inline";
   }
 }
 
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    // Call the function for each read more button
-    myFunction('dots1', 'more1', 'myBtn1');
-    myFunction('dots2', 'more2', 'myBtn2');
-    myFunction('dots3', 'more3', 'myBtn3');
-  });
-  
+document.addEventListener("DOMContentLoaded", function () {
+  // Call the function for each read more button
+  myFunction('dots1', 'more1', 'myBtn1');
+  myFunction('dots2', 'more2', 'myBtn2');
+  myFunction('dots3', 'more3', 'myBtn3');
+});
