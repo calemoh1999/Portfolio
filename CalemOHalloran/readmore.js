e0c6da1f0fa319fc0812234b57b66d99913f844a
calemoh1999 <calemohalloran99@gmail.com>
2023-08-07 20:34:01 +0100

    m

--- CalemOHalloran/readmore.js
+++ CalemOHalloran/readmore.js
@@ -1,18 +1,19 @@
 function myFunction(dotsId, moreTextId, btnId) {
-    var dots = document.getElementById(dotsId);
-    var moreText = document.getElementById(moreTextId);
-    var btnText = document.getElementById(btnId);
+  var dots = document.getElementById(dotsId);
+  var moreText = document.getElementById(moreTextId);
+  var btnText = document.querySelector("." + btnId); // Use querySelector to select by class name
   
-    if (dots.style.display === "none" || dots.style.display === "") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-    }
+  if (dots.style.display === "none" || dots.style.display === "") {
+    dots.style.display = "inline";
+    btnText.textContent = "Read more"; // Use textContent instead of innerHTML for simple text changes
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.textContent = "Read less"; // Use textContent instead of innerHTML for simple text changes
+    moreText.style.display = "inline";
   }
+}
+
   
   document.addEventListener("DOMContentLoaded", function () {
     // Call the function for each read more button
