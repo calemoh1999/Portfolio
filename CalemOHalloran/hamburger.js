ba699c67e9c2cc2cca003513f3f5af7a8b9470b2
calemoh1999 <calemohalloran99@gmail.com>
2023-08-22 15:50:11 +0100

    sack it we are done

--- CalemOHalloran/hamburger.js
+++ CalemOHalloran/hamburger.js
@@ -6,7 +6,6 @@ const navMenu = document.querySelector('.nav-menu');
 hamburgermenu.addEventListener('click', () => {
   hamburgermenu.classList.toggle('active');
   navMenu.classList.toggle('active');
-  document.body.classList.toggle("menu-open"); // Use document.body to refer to the body element
 })
 document.querySelectorAll(".nav-link").forEach(n => n.
 addEventListener("click", () => {
